e6cf66413739ea9db01a6d28b309c000250d68d4
Andrej <aangjushev@gmail.com>
2026-04-19 17:54:24 +0200

    Fix storage issue when deployed

--- red_devils_hub/settings.py
+++ red_devils_hub/settings.py
@@ -83,10 +83,14 @@ STATIC_ROOT      = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "core" / "static"]
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
 
 MEDIA_URL  = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
